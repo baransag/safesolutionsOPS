32acb3ddc182c60757b437c19d788dc2afc1a2ae
baransag <husnainfarooqhusnain912@gmail.com>
2026-08-04 15:10:06 +0500

    Update API URL

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,14 +2,14 @@ const { Pool } = require("pg");
 const env = require("./env");
 
 const pool = new Pool({
-  host:     env.DB_HOST,
-  port:     env.DB_PORT,
+  host: env.DB_HOST,
+  port: env.DB_PORT,
   database: env.DB_NAME,
-  user:     env.DB_USER,
+  user: env.DB_USER,
   password: env.DB_PASSWORD,
   max: 20,
-  idleTimeoutMillis:    30000,
-  connectionTimeoutMillis: 5000
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 5000,
 });
 
 pool.on("error", (err) => {
@@ -18,6 +18,6 @@ pool.on("error", (err) => {
 
 module.exports = {
   pool,
-  query:     (text, params) => pool.query(text, params),
-  getClient: () => pool.connect()
+  query: (text, params) => pool.query(text, params),
+  getClient: () => pool.connect(),
 };
